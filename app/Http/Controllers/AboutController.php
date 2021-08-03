669afaac491e508197b8958a95cb2c6211ce1f26
Serhii Baranovskyi <sbaranovskyy@gmail.com>
2021-08-04 00:45:10 +0300

    create_Controllers

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PhotoDescription extends Controller
+class AboutController extends Controller
 {
     /**
      * Display a listing of the resource.
